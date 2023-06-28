b459ceaf8d0d58300a3c81372d68a25d9a4822f8
Lenildo Luan <lenildoluan@gmail.com>
2023-06-28 11:25:40 -0300

    Edit main files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+import Header from './Header.js'
+import Nav from './Nav.js'
+import Main from './Main.js'
+import Footer from './Footer.js'
+
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
