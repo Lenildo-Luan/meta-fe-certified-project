7b7789ca662679df46516f55f28583d1cee59add
Lenildo Luan <lenildoluan@gmail.com>
2023-06-28 15:53:26 +0000

    Edit App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import './assets/styles/reset.css'
+import './assets/styles/typography.css'
 
-import Header from './Header.js'
+import Header from './components/header'
 import Nav from './Nav.js'
 import Main from './Main.js'
 import Footer from './Footer.js'
