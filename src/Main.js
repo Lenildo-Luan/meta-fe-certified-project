4c6c4174b44f638226e7a8bd9f94d885740bdb87
Lenildo Luan <lenildoluan@gmail.com>
2023-06-29 15:03:14 +0000

    Refactoring

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,10 @@
+import Hero from './components/hero'
+
 function Main() {
   return (
     <>
       <main>
-        <section>
-          <div>
-            <div>
-              <h1>Little Lemon</h1>
-              <h3>Chicago</h3>
-              <p>
-                We are a family owned Mediterranean restaurant, focused on traditional 
-                recipes served with a modern twist.
-              </p>
-              <button>Reserve a Table</button>
-            </div>
-
-            <img src="" alt=""/>
-          </div>
-        </section>
+        <Hero />
 
         <section>
           <div>
